9ce7b25b355ee2521e210db96a114dd295a20d51
MishaMiron <53948817+MykhailoEvtukh@users.noreply.github.com>
2020-01-29 13:01:23 +0200

    last commit before deploy

---
+++ build/precache-manifest.93ee950710af0b23f7fa10d2c3cbc7ef.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020a2975c0ae618db17edc0c95143b1c",
+    "url": "/react-movie-searcher/index.html"
+  },
+  {
+    "revision": "707efdd55d47568ef2ea",
+    "url": "/react-movie-searcher/static/css/4.b841cb64.chunk.css"
+  },
+  {
+    "revision": "173fe363f6c660ee02d0",
+    "url": "/react-movie-searcher/static/css/main.d1e7e54d.chunk.css"
+  },
+  {
+    "revision": "659c8e0689bbaf7e5847",
+    "url": "/react-movie-searcher/static/js/0.d5bd0e00.chunk.js"
+  },
+  {
+    "revision": "d6ff35d1d8d5384c0af4",
+    "url": "/react-movie-searcher/static/js/3.b49564e3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-movie-searcher/static/js/3.b49564e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "707efdd55d47568ef2ea",
+    "url": "/react-movie-searcher/static/js/4.c7183832.chunk.js"
+  },
+  {
+    "revision": "7e12009a97a2c3d627ed",
+    "url": "/react-movie-searcher/static/js/5.bd6e2622.chunk.js"
+  },
+  {
+    "revision": "083082efd4e7365dac43",
+    "url": "/react-movie-searcher/static/js/6.f15bad40.chunk.js"
+  },
+  {
+    "revision": "173fe363f6c660ee02d0",
+    "url": "/react-movie-searcher/static/js/main.425bd7fe.chunk.js"
+  },
+  {
+    "revision": "114c443df3fc9d8f22d9",
+    "url": "/react-movie-searcher/static/js/runtime-main.cf957c86.js"
+  }
+]);
