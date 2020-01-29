d7622c1ee3ea42fafca2f0929e7ceccb85afd064
MishaMiron <53948817+MykhailoEvtukh@users.noreply.github.com>
2020-01-29 12:51:52 +0200

    last commit before deploy

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React, { Suspense, lazy } from "react";
+import { Route, Switch } from "react-router-dom";
+import Navigation from "./Navigation/Navigation";
+import Loader from "react-loader-spinner";
+import routes from "../routes";
+
+const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("../pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+  import("../pages/MovieDetailsPage/MovieDetailsPage")
+);
+
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      <Suspense
+        fallback={
+          <Loader
+            type="Puff"
+            color="#aa77cc"
+            height={50}
+            width={50}
+            timeout={2000}
+          />
+        }
+      >
+        <Switch>
+          <Route exact path={routes.HOME} component={HomePage} />
+          <Route path={routes.MOVIES_DETAILS} component={MovieDetailsPage} />
+          <Route path={routes.MOVIES_PAGE} component={MoviesPage} />
+        </Switch>
+      </Suspense>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Navigation.module.css";
+
+import routes from "../../routes";
+
+const Navigation = () => {
+  return (
+    <ul className={styles.navigationList}>
+      <li>
+        <NavLink
+          className={styles.Link}
+          activeClassName={styles.activeLink}
+          to={routes.HOME}
+        >
+          HOME
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={styles.Link}
+          activeClassName={styles.activeLink}
+          to={routes.MOVIES_PAGE}
+        >
+          MOVIES
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default Navigation;
